a74852bc15d8d47b16f5c7ae5bb36bd846141403
Rodolfo <rodolfoafl@gmail.com>
2021-06-17 21:01:01 -0300

    Added first test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders all elements correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByLabelText("Número 1")).toBeInTheDocument();
+  expect(screen.getByLabelText("Número 2")).toBeInTheDocument();
+  expect(screen.getByText("Somar")).toBeInTheDocument();
 });
